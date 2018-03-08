254b3af684111548fb77c98c89a7dca64466c97a
Daring the CORD <dariuszbz@wp.pl>
2018-03-08 15:13:45 +0000

    architecture done. abstract and implementation done. mvc api and mvc web app in progress .....

---
+++ SoftwareAPI/Controllers/DocumentsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareAPI.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private static List<Models.Document> documents = new List<Models.Document>()
+        {
+            new Models.Document{ ActorID = Guid.NewGuid(), ActorPropID = Guid.NewGuid(), ActorPropTitle = "Doc1", Number = 1},
+            new Models.Document{ ActorID = Guid.NewGuid(), ActorPropID = Guid.NewGuid(), ActorPropTitle = "Doc2", Number = 2},
+            new Models.Document{ ActorID = Guid.NewGuid(), ActorPropID = Guid.NewGuid(), ActorPropTitle = "Doc3", Number = 3}
+        };
+        // GET: Documents
+        public ActionResult Index()
+        {
+            
+            return View(documents);
+        }
+
+        // GET: Documents/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(documents[id]);
+        }
+
+        // GET: Documents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Documents/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Documents/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(documents[id-1]);
+        }
+
+        // POST: Documents/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+             //   var ActorID = collection.GetValue("ActorID").RawValue;
+                var ActorPropTitle = collection.GetValue("ActorPropTitle").RawValue;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Documents/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SoftwareAPI/Models/Document.cs
+++ SoftwareAPI/Models/Document.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using ImplementationClasses;
@@ -8,8 +10,27 @@ namespace SoftwareAPI.Models
 {
     public class Document : ImplementationActorProp
     {
-        public Guid PersonID { get => base.ActorID; set => base.ActorID = value; }
-        public Guid ID { get => base.ActorPropID; set => base.ActorPropID = value; }
-        public string Title { get => base.ActorPropTitle; set => base.ActorPropTitle = value; }
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document Number")]
+        public int Number { get; set; }
+
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document Owner ID")]
+        public Guid ActorID { get => base.ActorID; set => base.ActorID = value; }
+
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document ID")]
+        public Guid ActorPropID { get => base.ActorPropID; set => base.ActorPropID = value; }
+
+        [DisplayName("Document Title")]
+        [StringLength(250, MinimumLength = 3)]
+        [Required(ErrorMessage = "Can't be empty or less than 3 characters")]
+        public string ActorPropTitle { get => base.ActorPropTitle; set => base.ActorPropTitle = value; }
+
+
     }
+
 }
